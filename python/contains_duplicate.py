310d2739d6a30055a8d280811fa2b4bca185e6c1
Bobby Coleman <bobbycoleman.dev@gmail.com>
2023-12-14 15:15:47 -0600

    first commit

---
+++ python/contains_duplicate.py
@@ -0,0 +1,19 @@
+nums1 = [1, 2, 3, 1]  # True
+nums2 = [1, 2, 3, 4]  # False
+nums3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]  # True
+
+
+def contains_duplicates(nums):
+    freq = {}
+    for i in range(len(nums)):
+        if str(nums[i]) in freq:
+            return True
+        else:
+            freq[str(nums[i])] = 0
+
+    return False
+
+
+print(contains_duplicates(nums1), "should equal True")
+print(contains_duplicates(nums2), "should equal False")
+print(contains_duplicates(nums3), "should equal True")
